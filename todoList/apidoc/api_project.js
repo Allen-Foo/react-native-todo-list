470a75969d5db90398b2959c755c07b2a4fb70f5
louislong <louislong@me.com>
2017-09-03 18:41:27 +0800

    [api-doc] explore the api doc

---
+++ todoList/apidoc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "todoList",  "version": "0.1.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-09-03T10:31:40.818Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
--- todoList/component/todoList.js
+++ todoList/component/todoList.js
@@ -38,12 +38,55 @@ class TodoList extends Component {
 	}
 }
 
+/**
+ * @api {get} /user/:id Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "firstname": "John",
+ *       "lastname": "Doe"
+ *     }
+ *
+ * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
 const mapStateToProps = (state) => {
 	return {
 		todos: state
 	}
 }
 
+/**
+ * @api {get} /login/randomkey Get random key 
+ * @apiName GetRandomkey
+ * @apiGroup Login
+ *
+ * @apiSuccess {String} randomekey the randomkey
+ * @apiSuccess {String} modulus 
+ * @apiSuccess {String} exponent
+ *
+ * @apiSuccessExample Success-Response: 
+ *     HTTP/1.1 200 OK
+ *     {
+ *      "randomekey": "John",
+ *       "modulus": "Doe",
+ *       "exponent": "Jack"
+ *     }
+ * 		
+ */
 const mapDispatchToProps = (dispatch) => {
 	return {
 		onTodoClick: id => {
